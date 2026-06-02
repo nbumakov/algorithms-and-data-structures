611cea767f90e0d592079046c8083d35dd9a2b39
Nikita Bumakov <nikita.bumakov@gmail.com>
2026-06-02 19:30:38 +0300

    Add Solution to t3633

---
+++ app/src/main/java/com/leetcode/t3633/Solution.kt
@@ -0,0 +1,27 @@
+package com.leetcode.t3633
+
+import kotlin.math.max
+import kotlin.math.min
+
+class Solution {
+    fun earliestFinishTime(
+        landStartTime: IntArray,
+        landDuration: IntArray,
+        waterStartTime: IntArray,
+        waterDuration: IntArray
+    ): Int {
+        val minLandEnd = landStartTime.indices.minOf { landStartTime[it] + landDuration[it] }
+        val minWaterEnd = waterStartTime.indices.minOf { waterStartTime[it] + waterDuration[it] }
+
+        // land first
+        val landFirst = waterStartTime.indices.minOf {
+            max(waterStartTime[it], minLandEnd) + waterDuration[it]
+        }
+        // water first
+        val waterFirst = landStartTime.indices.minOf {
+            max(landStartTime[it], minWaterEnd) + landDuration[it]
+        }
+
+        return min(landFirst, waterFirst)
+    }
+}
