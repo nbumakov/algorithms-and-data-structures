e4d6ad37d9755078717192b03719c49b88f4b3e4
Nikita Bumakov <nikita.bumakov@gmail.com>
2026-06-07 10:42:57 +0300

    update Solution 2196

--- app/src/main/java/com/leetcode/t2196/Solution.kt
+++ app/src/main/java/com/leetcode/t2196/Solution.kt
@@ -5,12 +5,14 @@ import com.leetcode.util.TreeNode
 class Solution {
     fun createBinaryTree(descriptions: Array<IntArray>): TreeNode? {
         val map = mutableMapOf<Int, TreeNode>()
-        val root = (descriptions.map { it[0] }.toSet() - descriptions.map { it[1] }.toSet()).first()
+        val children = HashSet<Int>()
         descriptions.forEach { (parent, child, isLeft) ->
+            children.add(child)
             val parentNode = map.getOrPut(parent) { TreeNode(parent) }
             val childNode = map.getOrPut(child) { TreeNode(child) }
             if (isLeft == 1) parentNode.left = childNode else parentNode.right = childNode
         }
+        val root = map.keys.first { it !in children }
         return map[root]
     }
 }
