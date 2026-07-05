22b0dab5b9b3592c04e5c5e1ca3ba7002e708ed5
Nikita Bumakov <nikita.bumakov@gmail.com>
2026-07-05 14:46:40 +0300

    add solution for 2492

--- app/src/main/java/com/leetcode/t2492/Solution.kt
+++ app/src/main/java/com/leetcode/t2492/Solution.kt
@@ -2,7 +2,7 @@ package com.leetcode.t2492
 
 import com.leetcode.util.print
 
-// Union find - Time Limit Exceeded
+// Union find with path compression only - Accepted
 class Solution {
     fun minScore(n: Int, roads: Array<IntArray>): Int {
         val uf = UnionFind(n)
@@ -10,23 +10,19 @@ class Solution {
             uf.union(road[0] - 1, road[1] - 1)
         }
 
-        var minScore = Int.MAX_VALUE
-        for (road in roads) {
-            if (uf.isConnected(0, road[0] - 1)) {
-                minScore = road[2].coerceAtMost(minScore)
-            }
+        return roads.minOf { (start, _, value) ->
+            value.takeIf { uf.isConnected(0, start - 1) } ?: Int.MAX_VALUE
         }
-
-        return minScore
     }
 
     private class UnionFind(size: Int) {
         private val root = IntArray(size) { it }
 
         fun find(x: Int): Int {
-            var x = x
-            while (x != root[x]) x = root[x]
-            return x
+            if (x != root[x]) {
+                root[x] = find(root[x])
+            }
+            return root[x]
         }
 
         fun union(x: Int, y: Int) {
@@ -40,12 +36,69 @@ class Solution {
     }
 }
 
+// Union find with path compression + union by rank - Accepted
+class Solution2 {
+    fun minScore(n: Int, roads: Array<IntArray>): Int {
+        val uf = UnionFind(n)
+        for (road in roads) {
+            uf.union(road[0] - 1, road[1] - 1)
+        }
+
+        return roads.minOf { (start, _, value) ->
+            value.takeIf { uf.isConnected(0, start - 1) } ?: Int.MAX_VALUE
+        }
+    }
+
+    private class UnionFind(size: Int) {
+        private val root = IntArray(size) { it }
+        private val rank = IntArray(size)
+
+        fun find(x: Int): Int {
+            var x = x
+            while (x != root[x]) {
+                root[x] = root[root[x]] // path halving
+                x = root[x]
+            }
+            return x
+        }
+
+        fun union(x: Int, y: Int) {
+            val rootX = find(x)
+            val rootY = find(y)
+            if (rootX == rootY) return
+            when {
+                rank[rootX] < rank[rootY] -> root[rootX] = rootY
+                rank[rootX] > rank[rootY] -> root[rootY] = rootX
+                else -> {
+                    root[rootY] = rootX
+                    rank[rootX]++
+                }
+            }
+        }
+
+        fun isConnected(x: Int, y: Int): Boolean =
+            find(x) == find(y)
+    }
+}
+
 fun main() {
     Solution()
-        .minScore(4, arrayOf(
-            intArrayOf(1, 2, 9),
-            intArrayOf(2, 3, 6),
-            intArrayOf(2, 4, 5),
-            intArrayOf(1, 4, 7)
-        )).print()
+        .minScore(
+            4, arrayOf(
+                intArrayOf(1, 2, 9),
+                intArrayOf(2, 3, 6),
+                intArrayOf(2, 4, 5),
+                intArrayOf(1, 4, 7)
+            )
+        ).print()
+
+    Solution2()
+        .minScore(
+            4, arrayOf(
+                intArrayOf(1, 2, 9),
+                intArrayOf(2, 3, 6),
+                intArrayOf(2, 4, 5),
+                intArrayOf(1, 4, 7)
+            )
+        ).print()
 }
