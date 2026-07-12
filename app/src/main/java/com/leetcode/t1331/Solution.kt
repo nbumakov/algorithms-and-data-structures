01dd59e993aca486c49614538f097ff1b522e333
Nikita Bumakov <nikita.bumakov@gmail.com>
2026-07-12 19:23:59 +0300

    Optimize LeetCode 1331: reduce allocations from 5 to 2, remove redundant toSet()

--- app/src/main/java/com/leetcode/t1331/Solution.kt
+++ app/src/main/java/com/leetcode/t1331/Solution.kt
@@ -8,3 +8,18 @@ class Solution {
         return arr.map { rankMap[it]!! }.toIntArray()
     }
 }
+
+class Solution2 {
+    fun arrayRankTransform(arr: IntArray): IntArray {
+        val sorted = arr.sortedArray()
+        val rankMap = HashMap<Int, Int>(sorted.size)
+        var rank = 0
+        for (v in sorted) {
+            if (v !in rankMap) {
+                rank++
+                rankMap[v] = rank
+            }
+        }
+        return IntArray(arr.size) { rankMap.getValue(arr[it]) }
+    }
+}
